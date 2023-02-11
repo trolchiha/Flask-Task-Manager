e7a36363d209c14ede00e6db4175a385561495f7
trolchiha <trotsko.olena@gmail.com>
2023-02-11 18:29:18 +0200

    code cleaning

--- app/models.py
+++ app/models.py
@@ -1,15 +1,14 @@
 from flask_login import UserMixin
 from . import db 
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000), nullable=False)
-    status = db.Column(db.Boolean, nullable=False, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150), nullable=False)
     tasks = db.relationship('Task', cascade="all, delete")
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000), nullable=False)
+    status = db.Column(db.Boolean, nullable=False, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
